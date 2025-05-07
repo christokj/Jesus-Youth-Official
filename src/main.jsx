919ad1d0f86aa2e2dca4854895abf882c7f07f72
Christo K J <christokj2@gmail.com>
2025-05-07 12:14:36 +0530

    Bug fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { store } from './redux/store.js'
-import { Provider } from 'react-redux'
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { store } from './redux/store.js';
+import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>,
-)
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
